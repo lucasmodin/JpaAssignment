79668c20fa58a6e17765841f867730aa16e935f2
lucasmodin <lumo0002@stud.kea.dk>
2025-02-03 14:35:22 +0100

    finish

--- src/main/java/org/example/studentjpa/controller/StudentController.java
+++ src/main/java/org/example/studentjpa/controller/StudentController.java
@@ -4,6 +4,7 @@ import org.example.studentjpa.model.Student;
 import org.example.studentjpa.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -29,19 +30,28 @@ public class StudentController {
         return studentRepository.save(student);
     }
 
-    @PutMapping("/student")
-    @ResponseStatus(HttpStatus.CREATED)
-    public Student updateStudent(@RequestBody Student student) {
-        Optional<Student> orgStudent = studentRepository.findById(student.getId());
+    @PutMapping("/student/{id}")
+    public ResponseEntity<Student> updateStudent(@PathVariable int id, @RequestBody Student student) {
+        Optional<Student> orgStudent = studentRepository.findById(id);
         if (orgStudent.isPresent()) {
+            student.setId(id);
             System.out.println(student);
-            return studentRepository.save(student);
+            studentRepository.save(student);
+            return new ResponseEntity<>(student, HttpStatus.OK);
         } else {
-            Student student1 = new Student();
-            student1.setName("NotFound");
-            return student1;
+            return new ResponseEntity<>(new Student(), HttpStatus.NOT_FOUND);
         }
+    }
 
+    @DeleteMapping("/student/{id}")
+    public ResponseEntity<String> deleteStudent(@PathVariable int id) {
+        Optional<Student> student = studentRepository.findById(id);
+        if (student.isPresent()) {
+            studentRepository.deleteById(id);
+            return ResponseEntity.ok("Student deleted");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Student not found");
+        }
     }
 
 
