4fdf000fc7d47a17c32c2918ab3f23d1c4b27fd0
lucasmodin <lumo0002@stud.kea.dk>
2025-02-03 13:17:56 +0100

    postman

--- src/main/java/org/example/studentjpa/controller/StudentController.java
+++ src/main/java/org/example/studentjpa/controller/StudentController.java
@@ -3,10 +3,11 @@ package org.example.studentjpa.controller;
 import org.example.studentjpa.model.Student;
 import org.example.studentjpa.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class StudentController {
@@ -18,6 +19,29 @@ public class StudentController {
     @GetMapping("/students")
     public List<Student> students() {
         return studentRepository.findAll();
+    }
+
+
+    @PostMapping("/student")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student postStudent(@RequestBody Student student) {
+        System.out.println(student);
+        return studentRepository.save(student);
+    }
+
+    @PutMapping("/student")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student updateStudent(@RequestBody Student student) {
+        Optional<Student> orgStudent = studentRepository.findById(student.getId());
+        if (orgStudent.isPresent()) {
+            System.out.println(student);
+            return studentRepository.save(student);
+        } else {
+            Student student1 = new Student();
+            student1.setName("NotFound");
+            return student1;
+        }
+
     }
 
 
